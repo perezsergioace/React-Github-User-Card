dcc685e625f467619bc3027580600b64f8ed7187
perezsergioace <perez.sergio.ace@gmail.com>
2019-12-03 19:46:01 -0500

    Fixed importing of my components

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-import Card from './components/Card'
+import Card from './components/Card';
+import Followers from './components/Followers';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Github User Card</h1>
-      <Card />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Github User Card</h1>
+			<Card />
+			<div>
+				<h1>Followers Card</h1>
+				<Followers />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import Followers from './Followers';
 
 class Card extends React.Component {
 	constructor() {
@@ -35,10 +34,6 @@ class Card extends React.Component {
 					<img src={this.state.cards.avatar_url} alt="Profile" />
 					<h2>Name: {this.state.cards.name}</h2>
 					<p>Username: {this.state.cards.login}</p>
-				</div>
-				<div>
-					<h1>Followers Card</h1>
-					<Followers />
 				</div>
 			</div>
 		);
